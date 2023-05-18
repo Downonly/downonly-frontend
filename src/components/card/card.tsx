4f4d7eeda3bec35d98d81ada4199d09cc3fd6874
Boris Diakur <borisdiakur@gmail.com>
2023-05-18 23:24:20 +0200

    ⬇️

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,48 +1,30 @@
-let componentCnt = 0
 export default function Card({ children }: { children: React.ReactNode }) {
-	componentCnt++
-	const filterId = `displace${componentCnt}`
-	const baseFrequency = Math.random() * 0.00025 + 0.00025
-	const numOctaves = Math.ceil(Math.random() * 3)
+	const size = 100
+	const strokeWidth = Math.round(size / 15)
+	const topLeftX = Math.round(strokeWidth + Math.random())
+	const topLeftY = Math.round(strokeWidth + Math.random())
+	const topRightX = Math.round(size - Math.random() - strokeWidth)
+	const topRightY = Math.round(strokeWidth + Math.random())
+	const bottomLeftX = Math.round(strokeWidth + Math.random())
+	const bottomLeftY = Math.round(size - Math.random() - strokeWidth)
+	const bottomRightX = Math.round(size - Math.random() - strokeWidth)
+	const bottomRightY = Math.round(size - Math.random() - strokeWidth)
 
 	return (
 		<div className="relative z-0 inline-flex p-6">
 			{children}
 			<svg
 				preserveAspectRatio="none"
-				className="absolute left-1/2 top-1/2 -z-10 inline-grid h-full w-full -translate-x-1/2 -translate-y-1/2 scale-105 transform"
-				viewBox="0 0 100 100"
+				className="absolute left-1/2 top-1/2 -z-10 inline-grid h-full w-full -translate-x-1/2 -translate-y-1/2 scale-x-110 scale-y-105 transform"
+				viewBox={`0 0 ${size} ${size}`}
 			>
-				<defs>
-					<filter id={filterId}>
-						<feTurbulence
-							type="turbulence"
-							baseFrequency={baseFrequency}
-							numOctaves={numOctaves}
-							result="turbulence"
-						/>
-						<feDisplacementMap
-							in2="turbulence"
-							in="SourceGraphic"
-							scale="8"
-							xChannelSelector="R"
-							yChannelSelector="G"
-						/>
-					</filter>
-				</defs>
-
-				<rect
-					className="dark:fill-carbon dark:stroke-carbon fill-white stroke-white stroke-2"
-					width="90"
-					height="90"
-					x="0"
-					y="1"
+				<path
+					className="dark:fill-carbon dark:stroke-carbon fill-white stroke-white"
 					style={{
-						filter: `url(#${filterId})`,
-						strokeWidth: '6',
-						strokeLinecap: 'square',
+						strokeWidth: strokeWidth,
 						strokeLinejoin: 'bevel',
 					}}
+					d={`M ${topLeftX} ${topLeftY} L ${topRightX} ${topRightY} L ${bottomRightX} ${bottomRightY} L ${bottomLeftX} ${bottomLeftY} Z`}
 				/>
 			</svg>
 		</div>
