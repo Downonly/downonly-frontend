912cf7fa16263f4a862615cffec36f5c2fb1f21f
Boris Diakur <borisdiakur@gmail.com>
2023-05-20 19:35:27 +0200

    ⬇️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,25 @@ import Card from '@/components/card/card'
 export default function Home() {
 	return (
 		<>
-			<div className="flex w-full flex-col justify-end sm:flex-row">
-				<div className="ms-[calc(-1*(50vw-min(35rem,45vw)))] aspect-4/3 w-screen justify-self-end bg-tomato sm:w-[50vw] sm:max-w-[40rem]" />
-				<div className="h-10 sm:w-1/2"></div>
+			<div className="flex w-full flex-col justify-end lg:flex-row">
+				<div className="ms-[calc(-1*(50vw-min(35rem,45vw)))] aspect-4/3 w-screen justify-self-end bg-tomato lg:w-[50vw] lg:max-w-[40rem]" />
+				<div className="flex items-center justify-center p-6 lg:w-1/2">
+					<div className="text-center">
+						<p className="mb-2">👮‍🏥🪑</p>
+						<p className="mb-2 font-display uppercase">
+							Mint #52
+							<br />
+							cop-hostital-chair-fall
+						</p>
+						<p className="text-carbon dark:text-iron">
+							Mint date: 01.01.2023
+							<br />
+							Lorem Ipsum
+							<br />
+							Price: 4.7 Eth
+						</p>
+					</div>
+				</div>
 			</div>
 
 			<Card className="w-full">
