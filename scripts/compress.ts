47156bc10c73270a8aa795a9ddfc46522c2f06cc
Boris Diakur <borisdiakur@gmail.com>
2024-11-03 22:54:25 +0100

    ⬇️

--- scripts/compress.ts
+++ scripts/compress.ts
@@ -4,32 +4,34 @@ import { processGlb } from 'gltf-pipeline'
 import glob from 'glob'
 import { readFile, writeFile } from 'fs/promises'
 
-/* eslint-disable @typescript-eslint/no-misused-promises */
 glob('./public/NewFallExamples/*.glb', (err, files) => {
 	if (err) throw err
 
 	files
 		.filter((file) => !file.includes('.draco'))
-		.forEach(async (file, i) => {
+		.forEach((file, i) => {
 			const iPadded = (i + 1 + '').padStart(2, '0')
-			const gltf = await readFile(file)
-			const options = {
-				resourceDirectory: `./public/NewFallExamples/${iPadded}/`,
-				dracoOptions: {
-					quantizeTexcoordBits: 0,
-					compressionLevel: 10,
-				},
-				binary: true,
-			}
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment
-			const result = await processGlb(gltf, options)
-			await writeFile(
-				`./public/NewFallExamples/${iPadded}.draco.glb`,
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
-				result.glb,
-				{
-					encoding: 'utf8',
-				}
-			)
+			void readFile(file)
+				.then((gltf) => {
+					const options = {
+						resourceDirectory: `./public/NewFallExamples/${iPadded}/`,
+						dracoOptions: {
+							quantizeTexcoordBits: 0,
+							compressionLevel: 10,
+						},
+						binary: true,
+					}
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+					return processGlb(gltf, options) as Promise<{ glb: string }>
+				})
+				.then(({ glb }) => {
+					return writeFile(
+						`./public/NewFallExamples/${iPadded}.draco.glb`,
+						glb,
+						{
+							encoding: 'utf8',
+						}
+					)
+				})
 		})
 })
