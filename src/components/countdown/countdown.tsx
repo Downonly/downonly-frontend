978dc1dc0d57fa9fd1933ffaf0ba536425492c11
Boris Diakur <borisdiakur@gmail.com>
2024-11-11 21:17:04 +0100

    ⬇️

--- src/components/countdown/countdown.tsx
+++ src/components/countdown/countdown.tsx
@@ -1,26 +1,25 @@
-import { type FC } from 'react'
+import { type FC, useEffect, useRef, useState } from 'react'
 import { formatDuration } from '@/utils/time'
 
 const Countdown: FC<{ seconds: number }> = ({ seconds }) => {
-	// const [localSeconds, setLocalSeconds] = useState(seconds)
-	// const timeout = useRef<number>()
-	//
-	// useEffect(() => {
-	// 	timeout.current = window.setTimeout(() => {
-	// 		if (localSeconds <= 1) {
-	// 			setLocalSeconds(seconds)
-	// 		} else {
-	// 			setLocalSeconds(Math.max(0, localSeconds - 1))
-	// 		}
-	// 	}, 1000)
-	//
-	// 	return () => {
-	// 		window.clearTimeout(timeout.current)
-	// 	}
-	// }, [localSeconds, seconds])
-	//
-	// return <>{formatDuration(localSeconds)}</>
-	return <>{formatDuration(seconds)}</>
+	const [localSeconds, setLocalSeconds] = useState(seconds)
+	const timeout = useRef<number>()
+
+	useEffect(() => {
+		timeout.current = window.setTimeout(() => {
+			if (localSeconds <= 1) {
+				setLocalSeconds(seconds)
+			} else {
+				setLocalSeconds(Math.max(0, localSeconds - 1))
+			}
+		}, 1000)
+
+		return () => {
+			window.clearTimeout(timeout.current)
+		}
+	}, [localSeconds, seconds])
+
+	return <>{formatDuration(localSeconds)}</>
 }
 
 export default Countdown
