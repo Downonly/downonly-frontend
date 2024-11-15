362b5d62db840a2b7799010e17a171ce7a42045f
Boris Diakur <borisdiakur@gmail.com>
2024-11-15 17:45:41 +0100

    ⬇️

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,7 +1,9 @@
 export function isDebug() {
+	if (typeof location === 'undefined') return false
 	return location.hash.includes('debug')
 }
 
 export function isPerf() {
+	if (typeof location === 'undefined') return false
 	return location.hash.includes('perf')
 }
