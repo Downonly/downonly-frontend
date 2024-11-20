3c40ad42ee7a93df8afd4ba4e097f4d1ae1043b7
Boris Diakur <borisdiakur@gmail.com>
2024-11-20 22:04:45 +0100

    ⬇️

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import localFont from 'next/font/local'
 import Nav from '@/components/nav/nav'
 import Footer from '@/components/footer/footer'
+import { Viewport } from 'next'
 
 const fontDisplay = localFont({
 	src: './jegelskerdig.woff2',
@@ -18,6 +19,12 @@ export const metadata = {
 		'Downonly is a time-limited performative work that combines an installation, an interactive game and a series of digital artworks. It gives participants a chance to engage in an experience centered around the concept of falling.',
 }
 
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	viewportFit: 'cover',
+}
+
 export default function RootLayout(props: { children: React.ReactNode }) {
 	return (
 		<html
@@ -25,6 +32,20 @@ export default function RootLayout(props: { children: React.ReactNode }) {
 			lang="en"
 			suppressHydrationWarning={true}
 		>
+			<head>
+				<link
+					rel="icon"
+					type="image/png"
+					href="/favicon-96x96.png"
+					sizes="96x96"
+				/>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
 			<body className="grid grid-rows-[auto_1fr_auto] bg-snow font-body text-carbon transition-colors dark:bg-cole dark:text-snow">
 				<Nav className="container relative z-10 my-10 w-full" />
 				<main>
