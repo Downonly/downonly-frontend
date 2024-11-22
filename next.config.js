88c1025beda04765b153f8b04e3e5bc7b0d404cc
Boris Diakur <borisdiakur@gmail.com>
2024-11-22 18:42:58 +0100

    ⬇️

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
 	output: 'export',
 	distDir: 'dist',
 	reactStrictMode: false,
-	basePath: process.env.MODE === 'gh_pages' ? '/downonly' : '',
+	basePath: process.env.MODE === 'gh_pages' ? '/downonly-frontend' : '',
+	assetPrefix: process.env.MODE === 'gh_pages' ? '/downonly-frontend' : '',
 }
 
 module.exports = nextConfig
