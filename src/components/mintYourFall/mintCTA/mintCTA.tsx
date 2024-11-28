61e2ac9251d202384cbf1329b3819b8b4b87b68f
Boris Diakur <borisdiakur@gmail.com>
2024-11-28 23:37:04 +0100

    ⬇️

--- src/components/mintYourFall/mintCTA/mintCTA.tsx
+++ src/components/mintYourFall/mintCTA/mintCTA.tsx
@@ -6,19 +6,18 @@ const MintCTA: FC<{
 }> = ({ selectedEmoji }): ReactElement => {
 	return (
 		<>
-			<div className="pt-6">
-				<div className="text-sm">
-					<div className="my-3">{selectedEmoji}</div>
-					<div className="text-display my-1 uppercase">---</div>
-					<div className="text-display mb-1 uppercase">🖥 ☠️</div>
-					<div className="text-xs">
-						<p className="text-display mb-1 uppercase">Auction is over</p>
-					</div>
-				</div>
-			</div>
-			<div>
-				<Button disabled className="relative z-10" salt={'cucumber'} size="lg">
-					Mint
+			<div className="text-display flex flex-col justify-center text-sm">
+				<div className="mt-3">{selectedEmoji}</div>
+				<div className="my-1">---</div>
+				<div>🖥 ☠️</div>
+				<p className="mb-4 text-xs">Auction is over</p>
+				<Button
+					disabled
+					className="relative z-10 self-center"
+					salt={'cucumber'}
+					size="lg"
+				>
+					<s>Mint</s>
 				</Button>
 			</div>
 		</>
